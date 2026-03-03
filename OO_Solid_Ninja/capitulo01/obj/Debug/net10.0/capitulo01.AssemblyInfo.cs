26fb0e777e8721e38f4f6887fc58ab3960b0ae1a
Renata Correa <renatacorrea@serel.com.br>
2026-03-03 14:01:23 -0300

    y

--- OO_Solid_Ninja/capitulo01/obj/Debug/net10.0/capitulo01.AssemblyInfo.cs
+++ OO_Solid_Ninja/capitulo01/obj/Debug/net10.0/capitulo01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("capitulo01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1342d1332dc7f4e7116c0a70faaae63ab2ece5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("capitulo01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("capitulo01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
